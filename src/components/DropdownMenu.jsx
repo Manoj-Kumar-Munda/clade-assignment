c500ec2adada1ac4072cc80d81ebf8ea3c743e7f
Manoj Kumar Munda <manojkrmunda828@gmail.com>
2024-06-26 17:52:52 +0530

    created dropdownMenu for sm devices

--- src/components/DropdownMenu.jsx
+++ src/components/DropdownMenu.jsx
@@ -1,13 +1,27 @@
-import React from "react";
+import React, { useEffect } from "react";
+import messages from "../assets/icons/message.png";
+import payments from "../assets/icons/payments.png";
+import application from "../assets/icons/application.png";
 
-const DropdownMenu = () => {
+const DropdownMenu = ({ isShow }) => {
+
   return (
-    <div className="absolute bg-white">
-      <div>Messages</div>
+    <div className={` md:hidden absolute shadow-btn rounded-lg w-max px-4 py-4 top-full right-2 bg-white border flex flex-col gap-2 transition delay-150 duration-300 ${ isShow ? "opacity-100  translate-y-1": "opacity-0 -translate-y-1"}`}>
+      <div className="flex gap-3 items-center">
+        <img className="w-4 h-4" src={messages} alt="messages" />
+        <span className="text-sm">Messages</span>
+      </div>
 
-      <div>Payments</div>
+      <div className="flex gap-3 items-center">
+        <img className="w-4 h-4" src={payments} alt="payments" />
+        <span className="text-sm">Payments</span>
+      </div>
 
-      <div>Application</div>
+      <div className="flex gap-3 items-center">
+        <img className="w-4 h-4" src={application} alt="application" />
+        <span className="text-sm">Application</span>
+
+      </div>
     </div>
   );
 };
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import briefcase from "../assets/icons/briefcase.png";
 import messages from "../assets/icons/message.png";
 import payments from "../assets/icons/payments.png";
@@ -6,7 +6,10 @@ import application from "../assets/icons/application.png";
 import notification from "../assets/icons/bellIcon.png";
 import logo from "../assets/icons/logo.png";
 import chevronDown from "../assets/icons/chevron-down.png";
+import DropdownMenu from "./DropdownMenu";
 const Header = () => {
+
+  const [showMenu, setShowMenu] = useState(false);
   return (
     <header className="px-4 py-2 flex justify-between shadow-header">
       <div className="bg-[#E7E7E7] flex justify-center items-center text-btn-primary-bg px-7 py-3 self-center">
@@ -43,9 +46,13 @@ const Header = () => {
       <div className="flex items-center justify-between gap-4">
         <img src={notification} className="w-6 h-6" />
 
-        <div className="flex items-center gap-2">
+        <div className="flex items-center gap-2 relative">
           <img src={logo} alt="logo" />
-          <img src={chevronDown} alt="" />
+          <img className="hidden md:block" src={chevronDown} alt="dropdown" />
+
+          <img className="md:hidden cursor-pointer" src={chevronDown} alt="dropdown" onClick={(e) => setShowMenu(prev => !prev)} />
+
+           <DropdownMenu isShow={showMenu} />
 
         </div>
 
